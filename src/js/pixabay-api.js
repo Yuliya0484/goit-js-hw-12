aabe5bb5d9d631b3b85ab1a89d2e1147752fa5ef
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2024-11-13 22:24:54 +0100

    done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import '../css/styles.css';
 const API_KEY = '47000249-41843abe91023242d8e7e2eb0';
 const BASE_URL = 'https://pixabay.com/api/';
 
@@ -13,18 +14,3 @@ export async function fetchImages(query, page, perPage = 15) {
     throw error;
   }
 }
-
-// export async function fetchImages(query, page = 1, perPage = 200) {
-//   const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
-//     query
-//   )}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-
-//   const response = await fetch(url);
-//   if (!response.ok) {
-//     throw new Error('Error fetching images');
-//   }
-
-//   const data = await response.json();
-//   return data.hits;
-// }
-// pixabay-api.js
