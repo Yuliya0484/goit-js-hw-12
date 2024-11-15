97466ecec6ab02ff6182095e04b91ddff0fbced8
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2024-11-15 09:24:53 +0100

    markup

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -1,4 +1,3 @@
-// render-functions.js
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -23,7 +22,7 @@ export function renderGallery(images) {
         <p><b>Likes:</b>${likes}</p>
         <p><b>Views:</b>${views}</p>
         <p><b>Comments:</b>${comments}</p>
-        <p><b>downLoads:</b>${downloads}</p>
+        <p><b>DownLoads:</b>${downloads}</p>
         </div>
       </a>
       `
@@ -41,35 +40,3 @@ export function renderGallery(images) {
 export function clearGallery() {
   document.querySelector('.gallery').innerHTML = '';
 }
-
-// export function renderGallery(images) {
-//   const galleryEl = document.querySelector('.gallery');
-//   const markup = images
-//     .map(
-//       ({
-//         webformatURL,
-//         largeImageURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) => `
-//     <a class="gallery__item" href="${largeImageURL}">
-//       <img src="${webformatURL}" alt="${tags}" width="360px" height="200px" loading="lazy"/>
-//       <div class="info">
-//         <p><b>Likes:</b> ${likes}</p>
-//         <p><b>Views:</b> ${views}</p>
-//         <p><b>Comments:</b> ${comments}</p>
-//         <p><b>Downloads:</b> ${downloads}</p>
-//       </div>
-//     </a>
-//   `
-//     )
-//     .join('');
-//   galleryEl.innerHTML = markup;
-// }
-
-// export function clearGallery() {
-//   document.querySelector('.gallery').innerHTML = '';
-// }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 // main.js
 import { fetchImages } from './js/pixabay-api.js';
 import { renderGallery, clearGallery } from './js/render-function.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-const searchForm = document.getElementById('search-form');
-const loadMoreBtn = document.getElementById('load-more');
+const searchForm = document.querySelector('#search-form');
+const loadMoreBtn = document.querySelector('#load-more');
 
 let query = '';
 let page = 1;
@@ -28,7 +30,10 @@ async function onSearch(event) {
     totalHits = data.totalHits;
 
     if (data.hits.length === 0) {
-      alert('No images found. Please try again.');
+      iziToast.warning({
+        title: 'Warning',
+        message: 'Please enter a search query.',
+      });
       return;
     }
 
@@ -38,7 +43,10 @@ async function onSearch(event) {
     }
   } catch (error) {
     console.error(error);
-    alert('Something went wrong. Please try again later.');
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Something went wrong. Please try again later.',
+    });
   }
 }
 
@@ -52,13 +60,20 @@ async function onLoadMore() {
     const totalPages = Math.ceil(totalHits / perPage);
     if (page >= totalPages) {
       loadMoreBtn.hidden = true;
-      alert("We're sorry, but you've reached the end of search results.");
+      iziToast.warning({
+        title: 'Success',
+        message: "We're sorry, but you've, reached the end of search results.",
+        position: 'topRight',
+        titleColor: '#808080',
+        messageColor: '#000000',
+      });
     }
-
     scrollPage();
   } catch (error) {
     console.error(error);
-    alert('Something went wrong. Please try again later.');
+    iziToast.warning({
+      message: 'Something went wrong. Please try again later.',
+    });
   }
 }
 
@@ -71,58 +86,3 @@ function scrollPage() {
     behavior: 'smooth',
   });
 }
-
-// import { fetchImages } from './js/pixabay-api';
-// import { renderGallery, clearGallery } from './js/render-function';
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// const formEl = document.querySelector('#search-form');
-// const loaderEl = document.querySelector('.loader');
-// let lightbox;
-
-// formEl.addEventListener('submit', async event => {
-//   event.preventDefault();
-
-//   const query = event.target.elements.searchQuery.value.trim();
-//   if (!query) {
-//     iziToast.warning({
-//       title: 'Warning',
-//       message: 'Please enter a search query.',
-//     });
-//     return;
-//   }
-
-//   clearGallery();
-//   loaderEl.style.display = 'block';
-
-//   try {
-//     const images = await fetchImages(query);
-
-//     loaderEl.style.display = 'none';
-//     if (images.length === 0) {
-//       iziToast.info({
-//         title: 'Info',
-//         message:
-//           'Sorry, there are no images matching your search query. Please try again!',
-//       });
-//     } else {
-//       renderGallery(images);
-
-//       // Ініціалізуємо або оновлюємо SimpleLightbox
-//       if (lightbox) {
-//         lightbox.refresh();
-//       } else {
-//         lightbox = new SimpleLightbox('.gallery a');
-//       }
-//     }
-//   } catch (error) {
-//     loaderEl.style.display = 'none';
-//     iziToast.error({
-//       title: 'Error',
-//       message: 'Something went wrong. Please try again later.',
-//     });
-//   }
-// });
